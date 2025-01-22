f2c6fcb846a99ff18ab6b5c0c56d11e3bfb727a3
ItayBe <itaybe777@gmail.com>
2025-01-22 21:01:38 +0200

    Add CORS

--- config/__init__.py
+++ config/__init__.py
@@ -2,3 +2,4 @@ from config.server import ServerSettings
 from config.postgres import PostgresSettings
 from config.authentication import AuthenticationSettings
 from config.redis import RedisSettings
+from config.cors import CORSSettings
