f01114a161e86c3d4222c5e77e54ddca0a2731b4
ItayBe <itaybe777@gmail.com>
2025-01-22 18:31:19 +0200

    Remove unnecessary content

--- exceptions/handlers/server.py
+++ exceptions/handlers/server.py
@@ -1,7 +1,6 @@
 from starlette import status
-from starlette.responses import JSONResponse
+from starlette.responses import Response
 
 
-def database_connection_error_handler() -> JSONResponse:
-    return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                        content=dict(message="Database connection error"))
+def database_connection_error_handler() -> Response:
+    return Response(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
