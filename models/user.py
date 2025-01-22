bdb055046a497fc506126e616a0834744fdc0e20
ItayBe <itaybe777@gmail.com>
2025-01-22 17:37:44 +0200

    Remove unnecessary user model

--- models/user.py
+++ models/user.py
@@ -1,13 +1,6 @@
-from pydantic import BaseModel, Field
-from uuid import uuid4
+from pydantic import BaseModel
 
 
 class User(BaseModel):
-    user_id: int = Field(default=uuid4().hex)
-    username: str
-    hashed_password: str
-
-
-class UserLogin(BaseModel):
     username: str
     password: str
